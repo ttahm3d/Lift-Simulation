92889e3157bd838de957651425a0ce2dfddab2f7
ttahm3d <ttahm3d@gmail.com>
2022-10-02 21:27:04 +0530

    feat: created basic layout

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+const floorsInput = document.querySelector("#number-of-floors");
+const liftsInput = document.querySelector("#number-of-lifts");
+const creationForm = document.querySelector(".form");
+const submitBtn = document.querySelector(".submitBtn");
+const liftSection = document.querySelector(".lift-simulation");
+
+creationForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  /**
+   *
+   * Generate lift layout
+   */
+});
+
+/**
+ *
+ * @param {Number} floors Number of floors
+ * @param {Number} lifts Number of lifts
+ */
+function createLiftAndFloors(floors, lifts) {}
